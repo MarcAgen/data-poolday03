97e17a24d1b8a158a019e625c47d5028aa2ea313
Marc Agen <marc.agen@epitech.eu>
2026-02-11 11:33:55 +0100

    [RM]: remove test from task01

--- task01.py
+++ task01.py
@@ -14,7 +14,3 @@ def import_data(db, data_path:str):
         with open(i, 'r') as file:
             filedata = json.load(file)
         db[i[0:-5]].insert_one(filedata)
-
-db = get_db ( "localhost" , 27017 , "nobel_database" )
-import_data ( db , "./ data" )
-print ( db . list_collection_names () )
